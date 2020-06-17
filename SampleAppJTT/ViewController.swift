98869b4479aa7863faa49cdaa2263db6249ca4c5
Nitin <>
2020-06-18 00:52:54 +0530

    Initial commit

---
+++ SampleAppJTT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleAppJTT
+//
+//  Created by Nitz on 18/06/20.
+//  Copyright © 2020 JTT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SampleAppJTTTests/SampleAppJTTTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleAppJTTTests.swift
+//  SampleAppJTTTests
+//
+//  Created by Nitz on 18/06/20.
+//  Copyright © 2020 JTT. All rights reserved.
+//
+
+import XCTest
+@testable import SampleAppJTT
+
+class SampleAppJTTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
